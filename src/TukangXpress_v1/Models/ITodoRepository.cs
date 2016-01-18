6a718487c71bcd2391acb8de10ea14d696d9d02c
Daniel Esaputra <Daniel Esaputra>
2016-01-18 17:51:18 +0700

    second

---
+++ src/TukangXpress_v1/Models/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TukangXpress_v1.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
---
+++ src/TukangXpress_v1/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TukangXpress_v1.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
